0955e5621529e04dc174b374abe25c760b169c1c
BhargavManiar <bhargav22478@gmail.com>
2020-10-31 18:54:12 +0000

    Added a file for solving a system of differential equations

---
+++ Calculus/SolvingSystemsOfDifferentialEquations.m
@@ -0,0 +1,24 @@
+syms u(t) v(t)
+
+ode1 = diff(u) == 3*u + 4*v;
+ode2 = diff(v) == -4*u + 3*v; 
+odes = [ode1; ode2]
+
+S = dsolve(odes)
+
+uSol(t) = S.u
+vSol(t) = S.v
+
+cond1 = u(0) == 0;
+cond2 = v(0) == 1;
+conds = [cond1; cond2];
+[uSol(t), vSol(t)] = dsolve(odes,conds)
+
+
+fplot(uSol)
+hold on
+
+fplot(vSol)
+grid on
+
+legend('uSol','VSol','Location', 'Best')
